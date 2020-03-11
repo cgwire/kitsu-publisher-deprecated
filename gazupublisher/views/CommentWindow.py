3905731797bbdb214c8e2376954d56d01519ed93
Romain Patalowski <ico.gro@laposte.net>
2020-03-11 15:17:40 +0100

    [NEW] First interface displaying tasks and enabling comments modification

---
+++ gazupublisher/views/CommentWindow.py
@@ -0,0 +1,47 @@
+import sys
+import gazu
+
+import Qt.QtWidgets as QtWidgets
+
+
+class CommentWindow(QtWidgets.QWidget):
+    """
+    A window that pops up when the user wants to enter a comment
+    """
+    def __init__(self, task, container):
+
+        super().__init__()
+
+        self.task = task
+        self.container = container
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QtWidgets.QPushButton('Send', self)
+        self.btn.move(105, 122.5)
+        self.btn.clicked.connect(self.sendComment)
+
+        self.le = QtWidgets.QTextEdit(self)
+        self.le.setFixedSize(290, 120)
+        # self.le.move(130, 22)
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Comment')
+        self.show()
+
+    def sendComment(self):
+        """
+        Send the comment and reload all the tasks
+        """
+        text = self.le.document().toPlainText()
+
+        if text:
+            gazu.task.add_comment(self.task, self.task["task_status_id"], text)
+            self.container.reload()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = CommentWindow("TestWindow")
+    app.exec_()
+    sys.exit(app.exec_())
