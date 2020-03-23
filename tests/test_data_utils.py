49a8687dc77b8b6043caa2674fd074d0d45b0248
Frank Rousseau <frank.rousseau@free.fr>
2020-03-23 14:57:20 +0100

    Put fixtures in a dedicated module

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -3,14 +3,12 @@ import gazu
 from unittest import mock
 from gazupublisher.utils import data as data_utils
 
-open_projects = [
-    {"name": "Caminandes", "id": "project-1"},
-    {"name": "Agent 327", "id": "project-2"}
-]
+from tests import fixtures
 
 gazu.project.all_open_projects = mock.MagicMock(
-    return_value=open_projects
+    return_value=fixtures.open_projects
 )
+
 
 def test_get_all_open_project_names():
     names = data_utils.get_all_open_project_names()
