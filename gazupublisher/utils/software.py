79a1545d876930f66cf0efc81246b505fda27ed0
Romain Patalowski <ico.gro@laposte.net>
2020-06-24 17:56:39 +0200

    [NEW] Add OpenGL and Cycle for Blender preview

--- gazupublisher/utils/software.py
+++ gazupublisher/utils/software.py
@@ -4,16 +4,36 @@ from different contexts.
 """
 
 
-def take_screenshot(output_path, extension):
+def take_render_screenshot(output_path, extension, use_viewtransform=True):
     """
-    Take a screenshot
+    Take a rendered screenshot
     """
     from gazupublisher.working_context import working_context
 
     if working_context == "BLENDER":
-        from gazupublisher.utils.blender import take_screenshot
+        from gazupublisher.utils.blender import (
+            take_render_screenshot,
+            set_current_color_space,
+        )
+        if not use_viewtransform:
+            set_current_color_space("Raw")
+        take_render_screenshot(output_path, extension)
+    elif working_context == "MAYA":
+        from gazupublisher.utils.maya import take_screenshot
 
         take_screenshot(output_path, extension)
+
+
+def take_viewport_screenshot(output_path, extension):
+    """
+    Take a viewport screenshot
+    """
+    from gazupublisher.working_context import working_context
+
+    if working_context == "BLENDER":
+        from gazupublisher.utils.blender import take_viewport_screenshot
+
+        take_viewport_screenshot(output_path, extension)
     elif working_context == "MAYA":
         from gazupublisher.utils.maya import take_screenshot
 
@@ -41,6 +61,16 @@ def list_cameras():
             ("CameraStandAlone1", "CameraObject1"),
             ("CameraStandAlone2", "CameraObject2"),
         ]
+
+
+def get_current_color_space():
+    import gazupublisher.working_context as w
+
+    if w.working_context == "BLENDER":
+        from gazupublisher.utils.blender import get_current_color_space
+
+        return get_current_color_space()
+
 
 def set_camera(camera):
     import gazupublisher.working_context as w
