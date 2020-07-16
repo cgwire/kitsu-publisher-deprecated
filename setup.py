cbc37d45e0649cbecee275acce4657267b6a5830
Romain Patalowski <ico.gro@laposte.net>
2020-07-16 18:20:29 +0200

    [FIX] Fix dependencies for Travis

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ install_requirements = [
     "gazu>=0.7.14",
     "qtazu@git+https://github.com/cgwire/qtazu.git#egg=qtazu",
     "qt.py@git+https://github.com/mottosso/Qt.py.git#egg=qt.py",
-    "dcc-utils@git+https://github.com/cgwire/dcc-utils.git#egg=dccutils"
 ]
 
 setup(
