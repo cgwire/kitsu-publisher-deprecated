3d60e8bf1adfe791426e13a4e2ab67aec7a23030
Frank Rousseau <frank.rousseau@free.fr>
2020-08-11 13:40:18 +0200

    [Feat] Use org information to convert estimation

--- gazupublisher/utils/date.py
+++ gazupublisher/utils/date.py
@@ -5,6 +5,7 @@ utility functions.
 """
 
 import datetime
+import pytz
 
 
 def compare_date(d1, d2):
@@ -45,17 +46,44 @@ def from_datetime_to_date(datetime_object):
     return datetime.datetime.date(datetime_object)
 
 
-def format_comment_date(comment_date):
+def format_comment_date(user, comment_date):
     """
     Return a more pleasant format for comment date display.
     """
     date = extract_date(comment_date)
+    date = utc_to_local(user, date)
     return date.strftime("%Y-%m-%d | %H:%M:%S")
 
 
-def format_table_date(table_date):
+def format_table_date(user, table_date):
     """
-    Return a more pleasant format for date and hour display.
+    Return a more pleasant format for date display.
     """
     date = extract_date(table_date)
+    date = utc_to_local(user, date)
     return date.strftime("%Y-%m-%d")
+
+
+def from_min_to_day(time, organisation=None):
+    """
+    Convert a str time with minutes as a unit (i.e. a task duration), into
+    another str rounded time with an 8-hour day as a unit.
+    If an organisation is given as parameter, we use its field `hours_by_day`
+    to know how long a working day lasts.
+    """
+    if organisation is not None:
+        hours_per_day = organisation["hours_by_day"] or 8
+    else:
+        hours_per_day = 8
+    day_estimation = int(time) / 60 / hours_per_day
+    return str(round(day_estimation, 1))
+
+
+def utc_to_local(user, utc_dt):
+    """
+    Turn a date into a date matching use timezone. It's useful when you need
+    to display a date.
+    """
+    local_tz = pytz.timezone(user["timezone"])
+    local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt)
